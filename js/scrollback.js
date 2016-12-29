6578d26b06121f1e1e87fea267d1e6696b1ddb34
Dongyun Jin <mylibero@gmail.com>
2016-12-28 21:02:19 -0800

    Fix a problem of deltaX and deltaY
    Signed-off-by: Dongyun Jin <mylibero@gmail.com>

--- js/scrollback.js
+++ js/scrollback.js
@@ -1,6 +1,6 @@
 document.addEventListener("wheel", function(e) {
-  if (e.shiftKey && e.deltaX != 0) {
-    window.history.go(-Math.sign(e.deltaX));
+  if (e.shiftKey && (e.deltaX != 0 || e.deltaY != 0)) {
+    window.history.go(-Math.sign(e.deltaX ? e.deltaX : e.deltaY));
     return e.preventDefault();
   }
 });
